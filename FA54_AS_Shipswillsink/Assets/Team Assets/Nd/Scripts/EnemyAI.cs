e2ba4c2cb919bbbd546ca197cc0789310658b81c
ndbiller <andie.biller@gmail.com>
2016-07-11 08:54:05 +0200

    zweiter versuch first build

--- FA54_AS_Shipswillsink/Assets/Team Assets/Nd/Scripts/EnemyAI.cs
+++ FA54_AS_Shipswillsink/Assets/Team Assets/Nd/Scripts/EnemyAI.cs
@@ -20,11 +20,11 @@ public class EnemyAI : MonoBehaviour {
 		int posX = 0;
 		int posY = 0;
 		while (!targetValid){
-			print ("AI is targeting up to x -> "+grid.GetUpperBound(0));
-			print ("AI is targeting up to y -> "+grid.GetUpperBound(1));
+			print ("AI is targeting up to x -> "+grid.GetUpperBound(0)+1);
+			print ("AI is targeting up to y -> "+grid.GetUpperBound(1)+1);
 			//get random x and y position
-			posX = Random.Range(0,grid.GetUpperBound(0));
-			posY = Random.Range (0,grid.GetUpperBound(1));
+			posX = Random.Range(0,grid.GetUpperBound(0)+1);
+			posY = Random.Range (0,grid.GetUpperBound(1)+1);
 			//see if field has been shot at before (is valid)
 			if (grid[posX, posY].GetComponent<WaterPosition>().hasBeenShotAt == false) {
 				targetValid = true;
@@ -32,6 +32,20 @@ public class EnemyAI : MonoBehaviour {
 		}
 		//shoot
 		print("Ka-Boom!");
+		WaitForSomeTimeAndShot(3, posX, posY);
+		Shoot(posX, posY);
+	}
+
+	void Shoot(int posX, int posY){
 		grid [posX, posY].GetComponent<WaterPosition> ().ShootAtField ();
+	}
+
+	IEnumerator WaitForSomeTimeAndShot(int time, int posX, int posY) {
+		print ("targeting...");
+		//print(Time.time);
+		yield return new WaitForSeconds(time);
+		//print(Time.time);
+		print("shooting...");
+		Shoot(posX, posY);
 	}
 }
